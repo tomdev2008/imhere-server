a0da6d82cd7d73e0e318f3b7ddfd2e6f41226f84
xiannenglin <xianneng.lin@gmail.com>
2014-11-17 19:58:49 +0800

    add locate request protos file

---
+++ src/main/java/org/xiaoxiancai/imhere/server/business/locate/LocateHandler.java
@@ -0,0 +1,25 @@
+/* 
+ **********************************************************************
+ * Copyright (c) 2014, xianneng.lin@gmail.com All Rights Reserved. 
+ **********************************************************************
+ */
+package org.xiaoxiancai.imhere.server.business.locate;
+
+import io.netty.channel.ChannelHandlerContext;
+
+import org.xiaoxiancai.imhere.server.business.BusinessHandler;
+
+/**
+ * 定位处理器
+ *
+ * @author xiannenglin
+ */
+public class LocateHandler extends BusinessHandler {
+
+	@Override
+	public void channelRead(ChannelHandlerContext ctx, Object msg)
+			throws Exception {
+		// TODO Auto-generated method stub
+		super.channelRead(ctx, msg);
+	}
+}
